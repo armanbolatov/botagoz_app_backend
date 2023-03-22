109ba8d19aef0e03bd7342c5e11d431777dd1f70
ltwmori <ltwmori@gmail.com>
2023-03-22 23:25:24 +0600

    feat: added requirements list

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import openai
 import pickle
 import replicate
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
 from pydantic import BaseModel
 from typing import Optional
+import cv2
+import numpy as np
+
 
 app = FastAPI()
 
 
-class Question(BaseModel):
-    question: str
-    user_id: Optional[str] = None
-
-
 @app.post("/answer_question/")
-async def answer_question_endpoint(question: Question, image: Optional[UploadFile] = None):
-    answer = answer_question(question.question, image.file if image else None, question.user_id)
-    return {"answer": answer}
+async def answer_question_endpoint(question: str = Form(...), user_id: str = Form(...), image: UploadFile = File(...)):
+    image_path = f"/storage/{user_id}.jpg"
+    image_data = np.frombuffer(await image.read(), np.uint8)
+    image_np = cv2.imdecode(image_data, cv2.IMREAD_COLOR)
+    cv2.imwrite(image_path, image_np)
+    answer_str = answer_question(question, image_path, user_id)
+    return {"answer": answer_str}
 
 
 def get_chat_log_path(user_id):
@@ -60,7 +62,7 @@ def correct_answer(question, blip_answer, chat_log_inline):
         max_tokens=64,
         messages=[
             {"role": "system", "content": "You are an AI assistant"},
-            {"role": "user", "content": f"We want to create an AI assistant to assist people with visual impairments by answering questions related to photos taken by the user. To accomplish this, we use the BLIP question-answering model. However, the model's responses are sometimes very brief, strange, and unpredictable. Your task is to write a proper reply to a user's prompt (User) based on BLIP's reply (BLIP) and nothing more. If you cannot provide a proper reply for any reason, just say 'I'm sorry. I cannot answer.' and nothing else. See the whole dialogue for reference:\n\n{chat_log_inline}\nBegin.\n\n" + QnA},
+            {"role": "user", "content": f"We want to create an AI assistant to assist people with visual impairments by answering questions related to photos taken by the user. To accomplish this, we use the BLIP question-answering model. However, the model's responses are sometimes very brief, strange, and unpredictable. Your task is to write a proper reply to a user's prompt (User) based on BLIP's reply (BLIP) and nothing more. If you cannot provide a proper reply for any reason, just say 'I'm sorry. I cannot answer.' and nothing else. Do not mention the BLIP in your response. See the whole dialogue for reference:\n\n{chat_log_inline}\nBegin.\n\n" + QnA},
         ],
         temperature=0.5,
     )["choices"][0]["message"]["content"]
@@ -125,3 +127,9 @@ def answer_question(question, image_path, user_id):
         pickle.dump(chat_log, f)
 
     return answer
+
+if __name__ == "__main__":
+    while True:
+        q = input("User: ")
+        answer = answer_question(q, "./image.jpg", "assel")
+        print(answer)
